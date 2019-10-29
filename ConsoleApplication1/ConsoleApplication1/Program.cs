5aea66596443160b1d448ed5fc9c36f0a08ea9ca
thanhphong <phongvo2728@gmail.com>
2019-10-29 16:38:24 +0700

    game

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+            int a = 0; int b = 100;
+            int count =1;
+            while (b > a + 1)
+            {
+                int c= (a+b)/2;
+                Console.Write("Q{0}: Is your munber less than {1}? ",count, c);
+                answer = Console.ReadLine();
+                if (answer == "y")
+                    b = c - 1;
+                if (answer == "N")
+                    a = c;
+                count++;
+            }
+            Console.Write("is your number {0}",a);
+            answer = Console.ReadLine();
+            if (answer == "y")
+                Console.WriteLine ("your number is{0}", a);
+            if (answer == "N")
+                Console.WriteLine ("your number is{0}", b);
+            }
+        }
+    }
+
